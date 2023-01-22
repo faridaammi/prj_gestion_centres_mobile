1d34dd07fe844fd757c4978abf822e11a5943b71
Oumaima Akhankham <Akhankhamouma@gmail.com>
2023-01-22 16:27:14 +0100

    style et activity xml de Verification mot de passe oublié

---
+++ app/src/main/java/com/example/prj_gestion_centre_mobile/Verification_mot_passe_oubli.java
@@ -0,0 +1,14 @@
+package com.example.prj_gestion_centre_mobile;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Verification_mot_passe_oubli extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_verification_mot_passe_oubli);
+    }
+}
