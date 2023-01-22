b09718260229fd0668537bf2b891b45e24debe2c
faridaammi <farid.tiznit.1999@gmail.com>
2023-01-22 23:31:21 +0100

    add signUp with this icons

--- app/src/main/java/com/example/prj_gestion_centre_mobile/MainActivity.java
+++ app/src/main/java/com/example/prj_gestion_centre_mobile/MainActivity.java
@@ -1,18 +1,34 @@
 package com.example.prj_gestion_centre_mobile;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.provider.Settings;
 import android.util.Log;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
 
 public class MainActivity extends AppCompatActivity {
+
+    BottomNavigationView nav;
+    Fragment farg =new Fragment();
+
+    AccueilFragment accueilFragment = new AccueilFragment();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Log.d("teste","testing push");
-        Log.d("teste1","testing push....");
+
+//        nav = findViewById(R.id.bottom_menu);
+//        farg = findNavController(R.id.IdContainer);
+
 
     }
 }
