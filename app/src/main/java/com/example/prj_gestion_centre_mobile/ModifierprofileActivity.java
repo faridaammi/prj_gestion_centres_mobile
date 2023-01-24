1024cab32d233ccee40cad4410d092012c8dc4c2
aminaelbayyadi <elbayyadiamina@gmail.com>
2023-01-24 18:20:49 +0100

    ModifierprofileActivity.java

---
+++ app/src/main/java/com/example/prj_gestion_centre_mobile/ModifierprofileActivity.java
@@ -0,0 +1,14 @@
+package com.example.prj_gestion_centre_mobile;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ModifierprofileActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_modifierprofile);
+    }
+}
