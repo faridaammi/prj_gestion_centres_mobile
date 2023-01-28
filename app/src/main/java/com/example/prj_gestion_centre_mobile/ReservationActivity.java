9d02950a22065693d0b0dc2563bd7c7e596018bd
Kaiji21 <elhaiba120@gmail.com>
2023-01-28 15:50:35 +0100

    Added custom number picker to Activity_reservation.xml and programmed button in ReservationActivity.java, also added resources to project;

--- app/src/main/java/com/example/prj_gestion_centre_mobile/ReservationActivity.java
+++ app/src/main/java/com/example/prj_gestion_centre_mobile/ReservationActivity.java
@@ -3,22 +3,34 @@ package com.example.prj_gestion_centre_mobile;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.CalendarView;
 import android.widget.TextView;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
 
 public class ReservationActivity extends AppCompatActivity {
     CalendarView calendarView;
-    TextView datedepart, datefin;
+    TextView datedepart, datefin, txtinvite;
+    Button btn_addinvite,btn_deleteinvite ;
+    int comptinvite=0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        String currentDate = new SimpleDateFormat("dd/MM/yyyy", Locale.getDefault()).format(new Date());
         setContentView(R.layout.activity_reservation);
+        btn_addinvite = (Button) findViewById(R.id.btnaddinvite);
+        btn_deleteinvite=(Button)findViewById(R.id.btndeleteinvite);
         calendarView= (CalendarView) findViewById(R.id.calendarView);
         datedepart= (TextView) findViewById(R.id.txt_datedepart);
         datefin = (TextView) findViewById(R.id.txt_datefin);
+        txtinvite = (TextView) findViewById(R.id.txt_invite);
+        datedepart.setText(new SimpleDateFormat("MMM").format(new Date())+new SimpleDateFormat("D").format(new Date()));
         calendarView.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
 
             @Override
@@ -34,5 +46,20 @@ public class ReservationActivity extends AppCompatActivity {
                 }
             }
         });
+
+    }
+
+
+
+    public void deleteinvite(View view) {
+        comptinvite--;
+        comptinvite = comptinvite<0?0:comptinvite;
+        txtinvite.setText(String.valueOf(comptinvite));
+    }
+
+    public void addinvite(View view) {
+        comptinvite++;
+        txtinvite.setText(String.valueOf(comptinvite));
+
     }
 }
