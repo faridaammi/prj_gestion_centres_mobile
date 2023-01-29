9f7bbd2b9bcaa492d4645f8b78b85c4e54be42b6
Kaiji21 <elhaiba120@gmail.com>
2023-01-29 16:12:37 +0100

    Modified Verification_motdepasse_oublie by programming button verifier to redirect user to accueil activity and added scroll view to Verification_motdepasse_oublie layout xml.

--- app/src/main/java/com/example/prj_gestion_centre_mobile/Verfication_motdepasse_oublieActivity.java
+++ app/src/main/java/com/example/prj_gestion_centre_mobile/Verfication_motdepasse_oublieActivity.java
@@ -2,13 +2,36 @@ package com.example.prj_gestion_centre_mobile;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Dialog;
+import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Verfication_motdepasse_oublieActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_verfication_motdepasse_oublie);
+
+    }
+
+    public void validermotdepasse(View view) {
+        Dialog dialog = new Dialog(this);
+        dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+        dialog.setContentView(R.layout.activity_popup_felicitation);
+        Button btn= (Button) dialog.findViewById(R.id.btnrederiction);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(),AccueilActivity.class);
+                startActivity(intent);
+            }
+        });
+        dialog.show();
+
     }
 }
