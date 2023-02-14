e019b0ddac07251bd34488b0e956acc4a8099470
codingirlma <asmaounajaren@gmail.com>
2023-02-14 15:55:57 +0100

    add method validation password and email.

--- app/src/main/java/com/example/prj_gestion_centre_mobile/SignIn.java
+++ app/src/main/java/com/example/prj_gestion_centre_mobile/SignIn.java
@@ -28,10 +28,10 @@ import java.util.regex.Pattern;
 import cz.msebera.android.httpclient.Header;
 
 public class SignIn extends AppCompatActivity {
-    final String url_login ="http://192.168.43.45:8000/api/login";
+    final String url_login ="http://192.168.0.107:8000/api/login";
     public static String nom_organisme;
     Button btn_identifier;
-    EditText txt_password,txt_username;
+    EditText txt_password,txt_email;
     TextView txt_Sinscrire;
     String email,password;
     @Override
@@ -46,7 +46,7 @@ public class SignIn extends AppCompatActivity {
 
 
         btn_identifier = findViewById(R.id.btn_sidentifier);
-        txt_username = findViewById(R.id.IdEmail);
+        txt_email = findViewById(R.id.IdEmail);
         txt_password = findViewById(R.id.IdPassword);
         txt_Sinscrire = findViewById(R.id.txt_Sinscrire);
 
@@ -61,75 +61,84 @@ public class SignIn extends AppCompatActivity {
         btn_identifier.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                validateEmail(txt_username);
+                if(validateEmail(txt_email)&& validatePassword()){
+                    Signin();
+                }
             }
         });
-//        Intent intent = getIntent();
-////        email = intent.getStringExtra("email");
-////        password = intent.getStringExtra("password");
-//        txt_username.setText(intent.getExtras().get("email").toString());
-//        txt_password.setText(intent.getExtras().get("password").toString());
     }
 
     public boolean validateEmail(EditText emailtxt)
     {
         String emailInput = emailtxt.getText().toString();
-        password = txt_password.getText().toString();
-        if(!emailInput.isEmpty() && Patterns.EMAIL_ADDRESS.matcher(emailInput).matches())
-        {
-            ProgressDialog dialog = new ProgressDialog(this);
-            dialog.setMessage("Login.....");
-            dialog.show();
-            try {
-                RequestParams params= new RequestParams();
-                params.put("Login",emailInput);
-                params.put("Password",password);
-             new AsyncHttpClient().get(url_login,params, new AsyncHttpResponseHandler() {
-                 @Override
-                 public void onSuccess(int statusCode, Header[] headers, byte[] responseBody) {
-                     try {
-                         JSONObject object = new JSONObject(new String(responseBody));
-                         Log.d("datass", object.toString());
-                         dialog.dismiss();
-                         if (object.getString("message").equals("Kayn had luser")){
-                             JSONObject user = new JSONObject(object.getString("Utilisateur"));
-                             nom_organisme=user.getString("nom_Organisme");
-                             Intent intent = new Intent(SignIn.this,AccueilActivity.class);
-                             intent.putExtra("ID_organisme",user.getString("id_Organisme"));
-                             startActivity(intent);
-                         }
-                         else {
-                             Log.d("makaynch", "makaynchhhh");
-                             emailtxt.setError("Cet utilisateur n'existe pas");
-
-                         }
-
-                     } catch (JSONException e) {
-                         e.printStackTrace();
-                     }
-
-
-                 }
-
-                 @Override
-                 public void onFailure(int statusCode, Header[] headers, byte[] responseBody, Throwable error) {
-                   Log.e("error",error.getMessage());
-                 }
-             });
-
-            }
-            catch (Exception ex){
-                Log.e("Error",ex.getMessage());
-            }
-
-            return true;
+        if(emailInput.isEmpty()){
+            emailtxt.setError("Veuillez saisir votre email");
+            return false;
+        } else if (!Patterns.EMAIL_ADDRESS.matcher(emailInput).matches()) {
+            emailtxt.setError("Votre email est incorrect");
+            return false;
         }
         else
         {
-
-            Toast tost = Toast.makeText(this,"Votre Email Incorrect",Toast.LENGTH_SHORT);
-            tost.show();
+            return true;
+        }
+    }
+    private boolean validatePassword() {
+        String passwordInput = txt_password.getText().toString();
+        if (passwordInput.isEmpty()) {
+            txt_password.setError("Ce champs est obligatoire ");
             return false;
+        }
+        if (passwordInput.length() < 5) {
+            txt_password.setError("le mot passe doit contenir au moins 5 caracteres");
+            return false;
+        } else {
+            return true;
+        }
+    }
+    public void Signin(){
+        String emailInput = txt_email.getText().toString();
+        password = txt_password.getText().toString();
+        ProgressDialog dialog = new ProgressDialog(this);
+        dialog.setMessage("Login.....");
+        dialog.show();
+        try {
+            RequestParams params= new RequestParams();
+            params.put("Login",emailInput);
+            params.put("Password",password);
+            new AsyncHttpClient().get(url_login,params, new AsyncHttpResponseHandler() {
+                @Override
+                public void onSuccess(int statusCode, Header[] headers, byte[] responseBody) {
+                    try {
+                        JSONObject object = new JSONObject(new String(responseBody));
+                        Log.d("datass", object.toString());
+                        dialog.dismiss();
+                        if (object.getString("message").equals("Kayn had luser")){
+                            JSONObject user = new JSONObject(object.getString("Utilisateur"));
+                            nom_organisme=user.getString("nom_Organisme");
+                            Intent intent = new Intent(SignIn.this,AccueilActivity.class);
+                            intent.putExtra("ID_organisme",user.getString("id_Organisme"));
+                            startActivity(intent);
+                        }
+                        else {
+                            Log.d("makaynch", "makaynchhhh");
+                            txt_email.setError("Cet utilisateur n'existe pas");
+
+                        }
+
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                }
+
+                @Override
+                public void onFailure(int statusCode, Header[] headers, byte[] responseBody, Throwable error) {
+                    Log.e("error",error.getMessage());
+                }
+            });
+        }
+        catch (Exception ex){
+            Log.e("Error",ex.getMessage());
         }
     }
 }
